4b3b55e76a06a94515fc45fc03f3b1824720146b
Kassa-hash <kassa6173@gmail.com>
2026-02-17 21:07:48 +0300

    login

---
+++ frontend/cv-front/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Login from './components/Login.vue';
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <router-view />
+</template>
+
+
